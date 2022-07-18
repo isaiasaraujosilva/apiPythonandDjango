f2d171e069563b3b65133dff32f1f5cc3da44651
isaias-araujo1 <isaias-araujo1@hotmail.com>
2022-07-18 20:57:08 -0300

    create table cliente

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class cliente(models.Model):
+    primeiroNome = models.CharField(max_length=30)
+    segundoNome = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    telefone = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
