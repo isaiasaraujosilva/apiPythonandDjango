bab3665131758772adf01d4e0246ca374125494c
isaias-araujo1 <isaias-araujo1@hotmail.com>
2022-07-18 22:33:49 -0300

    create router produto

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,12 @@ from atexit import register
 from django.contrib import admin
 from django.db import router
 from django.urls import path,include
-from api.views import ClienteViewSet
+from api.views import ClienteViewSet, ProdutoViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'cliente', ClienteViewSet)
+router.register(r'produto', ProdutoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
