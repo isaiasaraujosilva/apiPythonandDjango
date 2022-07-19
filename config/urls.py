805b356fad0e7ebf54d0cfcc65dfb9896e8ee25b
isaias-araujo1 <isaias-araujo1@hotmail.com>
2022-07-18 23:05:03 -0300

    create router pedido

--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,13 @@ from atexit import register
 from django.contrib import admin
 from django.db import router
 from django.urls import path,include
-from api.views import ClienteViewSet, ProdutoViewSet
+from api.views import ClienteViewSet, ProdutoViewSet, PedidoViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'cliente', ClienteViewSet)
 router.register(r'produto', ProdutoViewSet)
+router.register(r'pedido', PedidoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
