29001f7f8db8025a022b79d187b9842a86ffc194
isaias-araujo1 <isaias-araujo1@hotmail.com>
2022-07-18 21:43:46 -0300

    import serializer create class ClienteViewSet

--- api/views.py
+++ api/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from api.models import cliente
+from api.serializer import ClienteSerializador
+
 # Create your views here.
+
+class ClienteViewSet(viewsets.ModelViewSet):
+    queryset = cliente.objects.all()
+    serializer_class = ClienteSerializador
 
 
