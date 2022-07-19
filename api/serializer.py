938db376c73d291aabaeaba9cd3424d0329ac590
isaias-araujo1 <isaias-araujo1@hotmail.com>
2022-07-18 22:33:14 -0300

    create class ProdutoSerializador

--- api/serializer.py
+++ api/serializer.py
@@ -1,8 +1,12 @@
 from dataclasses import fields
 from rest_framework import serializers
-from api.models import cliente
+from api.models import cliente, produto
     
 class ClienteSerializador(serializers.ModelSerializer):
     class Meta:
         model = cliente
         fields = ['id','nome','sobrenome','email','telefone']
+class ProdutoSerializador(serializers.ModelSerializer):
+    class Meta:
+        model = produto
+        fields = ['id','nome_do_produto','marca','unidade']
