43b0685903db966b674eb05f91f9b57d6547a9a0
isaias-araujo1 <isaias-araujo1@hotmail.com>
2022-07-18 21:23:58 -0300

    add 'table Clientes' display

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from api.models import cliente
 # Register your models here.
+class clientes(admin.ModelAdmin):
+    list_display = ('id','nome','sobrenome','email','telefone')
+    list_display_links = ('id','nome','sobrenome','email','telefone')
+    search_fields = ['nome']
+
+admin.site.register(cliente,clientes) 
