dcf2bab4c72dbfc986902e34bfc61fa977264443
isaias-araujo1 <isaias-araujo1@hotmail.com>
2022-07-18 22:33:34 -0300

    create class ProdutoViewSet

--- api/views.py
+++ api/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from api.models import cliente
-from api.serializer import ClienteSerializador
+from api.models import cliente, produto
+from api.serializer import ClienteSerializador, ProdutoSerializador
 
 # Create your views here.
 
 class ClienteViewSet(viewsets.ModelViewSet):
     queryset = cliente.objects.all()
     serializer_class = ClienteSerializador
+
+class ProdutoViewSet(viewsets.ModelViewSet):
+
+    queryset = produto.objects.all()
+    serializer_class = ProdutoSerializador
 
 
